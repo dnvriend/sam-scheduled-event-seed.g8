7e8c969802153b8580367ab2b56b42181b4dc730
Dennis Vriend <dnvriend@gmail.com>
2017-12-24 14:39:13 +0100

    v1.0.1

--- src/main/g8/src/main/scala/$package$/PersonRepository.scala
+++ src/main/g8/src/main/scala/$package$/PersonRepository.scala
@@ -4,7 +4,6 @@ import java.util.UUID
 
 import com.amazonaws.services.dynamodbv2.model.{AttributeValue, PutItemRequest, ReturnValue}
 import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClientBuilder}
-import com.amazonaws.services.lambda.runtime.Context
 import com.github.dnvriend.lambda._
 import com.github.dnvriend.lambda.annotation.{HttpHandler, ScheduleConf}
 import play.api.libs.json.{Json, _}
@@ -16,11 +15,8 @@ object Person {
 }
 final case class Person(name: String, id: Option[String] = None)
 
-object PersonRepository {
-  final val TableName = {
-    val projectName = sys.env("PROJECT_NAME")
-    val stage = sys.env("STAGE")
-  }
+class PersonRepository(tableName: String, ctx: SamContext) {
+  val table: String = ctx.dynamoDbTableName(tableName)
   val db: AmazonDynamoDB = AmazonDynamoDBClientBuilder.defaultClient()
 
   def id: String = UUID.randomUUID.toString
@@ -28,7 +24,7 @@ object PersonRepository {
   def put(id: String, person: Person): Unit = {
     db.putItem(
       new PutItemRequest()
-        .withTableName(TableName)
+        .withTableName(table)
         .withReturnValues(ReturnValue.NONE)
         .withItem(
           Map(
@@ -40,13 +36,13 @@ object PersonRepository {
   }
 
   def get(id: String): Person = {
-    val json = db.getItem(TableName, Map("id" -> new AttributeValue(id)).asJava)
+    val json = db.getItem(table, Map("id" -> new AttributeValue(id)).asJava)
       .getItem.get("json").getS
     Json.parse(json).as[Person]
   }
 
   def list: List[Person] = {
-    db.scan(TableName, List("json").asJava)
+    db.scan(table, List("json").asJava)
       .getItems.asScala.flatMap(_.values().asScala).map(_.getS).toList
       .map(Json.parse)
       .map(_.as[Person])
@@ -55,35 +51,40 @@ object PersonRepository {
 
 @ScheduleConf(schedule = "rate(1 minute)")
 class PutPersonScheduled extends ScheduledEventHandler {
-  override def handle(event: ScheduledEvent, ctx: Context): Unit = {
-    val id: String = PersonRepository.id
-    PersonRepository.put(id, Person("schedule", Option(id)))
+  override def handle(event: ScheduledEvent, ctx: SamContext): Unit = {
+    val repo = new PersonRepository("person", ctx)
+    val id: String = repo.id
+    val person = Person("schedule", Option(id))
+    repo.put(id, person)
   }
 }
 
 @HttpHandler(path = "/person", method = "post")
 class PostPerson extends ApiGatewayHandler {
-  override def handle(request: HttpRequest, ctx: Context): HttpResponse = {
-    val id: String = PersonRepository.id
+  override def handle(request: HttpRequest, ctx: SamContext): HttpResponse = {
+    val repo = new PersonRepository("person", ctx)
+    val id: String = repo.id
     val person = request.bodyOpt[Person].get
-    PersonRepository.put(id, person)
+    repo.put(id, person)
     HttpResponse.ok.withBody(Json.toJson(person.copy(id = Option(id))))
   }
 }
 
 @HttpHandler(path = "/person", method = "get")
 class GetListOfPerson extends ApiGatewayHandler {
-  override def handle(request: HttpRequest, ctx: Context): HttpResponse = {
-    HttpResponse.ok.withBody(Json.toJson(PersonRepository.list))
+  override def handle(request: HttpRequest, ctx: SamContext): HttpResponse = {
+    val repo = new PersonRepository("person", ctx)
+    HttpResponse.ok.withBody(Json.toJson(repo.list))
   }
 }
 
 @HttpHandler(path = "/person/{id}", method = "get")
 class GetPerson extends ApiGatewayHandler {
-  override def handle(request: HttpRequest, ctx: Context): HttpResponse = {
+  override def handle(request: HttpRequest, ctx: SamContext): HttpResponse = {
+    val repo = new PersonRepository("person", ctx)
     request.pathParamsOpt[Map[String, String]].getOrElse(Map.empty).get("id")
       .fold(HttpResponse.notFound.withBody(Json.toJson("Person not found")))(id => {
-        HttpResponse.ok.withBody(Json.toJson(PersonRepository.get(id)))
+        HttpResponse.ok.withBody(Json.toJson(repo.get(id)))
       })
   }
 }
